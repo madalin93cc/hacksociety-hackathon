ce586fc9900f2b641fc18cd2d742f07dc2f8d4c2
madalin.colezea <madalin.colezea@ymens.com>
2016-12-09 20:01:28 +0200

    tinder

--- src/main/webapp/app/home/home.controller.js
+++ src/main/webapp/app/home/home.controller.js
@@ -35,9 +35,35 @@
             title: 'Clara Oswin Oswald',
             subtitle: 'clara@gmail.com'
         }, {
-            thumbnail: 'plm.jpg',
+            thumbnail: 'https://images-na.ssl-images-amazon.com/images/M/MV5BMTc0MDMyMzI2OF5BMl5BanBnXkFtZTcwMzM2OTk1MQ@@._V1_UX214_CR0,0,214,317_AL_.jpg',
             title: 'Emy Pond',
             subtitle: 'emy@gmail.com'
-        }]
+        }];
+        vm.size = {
+            width: 300,
+            height: 400
+        };
+
+        vm.myCustomFunction = function () {
+
+        };
+
+        vm.showinfo = true;
+
+        vm.swipeend = function(){
+            vm.actions.unshift({name: 'Collection Empty'});
+        };
+
+        vm.clickedTimes = 0;
+
+        vm.actions = [];
+
+        vm.swipeLeft = function(person){
+            vm.actions.unshift({name: 'Left swipe'});
+        };
+
+        vm.swipeRight = function(person){
+            vm.actions.unshift({name: 'Right swipe'});
+        };
     }
 })();
