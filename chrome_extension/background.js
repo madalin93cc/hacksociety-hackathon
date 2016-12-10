03d349b2965500f7ab5cf5638da433713d42f933
romeo <romeo.tudureanu@gmail.com>
2016-12-10 08:56:58 +0200

    Add chrome extension

---
+++ chrome_extension/background.js
@@ -0,0 +1,16 @@
+// chrome.browserAction.onClicked.addListener(function(tab) {
+chrome.webNavigation.onCompleted.addListener(function(details) {
+  console.log(details);
+// chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab){
+  if(details.frameId == 0) {
+    chrome.tabs.executeScript({
+      file: "insert.js"
+    });
+  }
+}, {
+  url: [
+    {hostContains: '.apple.'},
+    {hostContains: '.google.'},
+    {hostContains: '.microsoft.'},
+    ]
+});
